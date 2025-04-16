4fb98f132a5892f91e18e6d84402a881198308b4
SowjanyaMaddhipatla <sowjanyadevimaddhipatla@gmail.com>
2025-04-16 16:15:03 +0530

    Incorrect_Content-Type_method_is_fixed_in_items.js

--- Buggy_Repo/frontend/scripts/items.js
+++ Buggy_Repo/frontend/scripts/items.js
@@ -42,7 +42,9 @@ document.getElementById("itemForm").addEventListener("submit", async (e) => {
   const description = document.getElementById("description").value;
   await fetch(`${baseURL}/items`, {
     method: "POST",
-    headers: { "Content-Type": "application/html" },
+    //headers: { "Content-Type": "application/html" },
+    //changed into json
+    headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ name, description })
   });
   e.target.reset();
